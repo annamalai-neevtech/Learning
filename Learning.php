547216cc77d475a2569668e222349c54091979ef
annamalai-neevtech <annamalai.somasundaram@neevtech.com>
2015-04-11 10:16:24 +0530

    2. Find the Largest Number in the array without using MAX Function

--- Learning.php
+++ Learning.php
@@ -8,3 +8,22 @@
    echo $missingNumber = $total - array_sum($numbers);
 
 ?>
+
+<?php 
+//2. Find the Largest Number in the array without using MAX Function 
+$i=0;
+$array=array(1,656,77,9349,121,11101,233,3);
+$finalValue=0;
+foreach($array as $k => $c)
+{
+
+$nextValue=$i+1;
+if(isset($array[$nextValue]) &&( $c > $array[$nextValue] ))
+{
+unset($array[$k]);
+$array[$nextValue]=$c;
+$finalValue=$c;
+}
+}
+echo $finalValue;
+?>
